4e6065c402c6c9c78b5453d70e3170a8b1fc7947
LykiFyar <joaopvbbraga@gmail.com>
2023-03-12 13:41:08 +0000

    TPC3 feito (late but done)

--- TPC3/mypages.js
+++ TPC3/mypages.js
@@ -15,6 +15,9 @@ exports.genMainPage = function(list, date) {
             <div class="w3-card-4">
                 <header class="w3-container w3-purple">
                     <h1>Lista de Pessoas</h1>
+                    <a class="w3-btn w3-round" href="/pessoas/sexo">Distribuição por sexo</a>
+                    <a class="w3-btn w3-round" href="/pessoas/desporto">Distribuição por desporto</a>
+                    <a class="w3-btn w3-round" href="/pessoas/profissoes">Top 10 Profissões</a>
                 </header>
             
             <div class="w3-container">
@@ -72,11 +75,28 @@ exports.genPersonPage = function(p,d) {
             <div class="w3-card-4">
                 <header class="w3-container w3-purple">
                     <h1>${p.nome}</h1>
+                    <a class="w3-btn w3-round w3-white" href="/">Página Inicial</a>
                 </header>
                 <div class "w3-container">
+                    <ul class="w3-ul w3-card-4">
+                        <li><b>Idade: </b>${p.idade}</li>
+                        <li><b>Sexo: </b>${p.sexo}</li>
+                        <li><b>Cidade: </b>${p.morada.cidade}</li>
+                        <li><b>Distrito: </b>${p.morada.distrito}</li>
+                        <li><b>BI/CC: </b>${p.BI || p.CC}</li>
+                        <li><b>Profissão: </b>${p.profissao}</li>
+                        <li><b>Desportos: </b>
+                            <ul class="w3-ul w3-card-4">
+                        `
+        for(sport of p.desportos) {
+            pagHTML += "<li><b>" + sport + "</li>"
+        }
 
-                    <p>Preencher os restantes campos</p>
-                
+        pagHTML += 
+                        `       </ul>
+                            </li>
+                        <li><b>More info to add..</b></li>
+                    </ul>
                 </div>
                 <footer class="w3-container w3-blue">
                         <h5>Generated in EngWeb2023 ${d}</h5>
@@ -84,6 +104,409 @@ exports.genPersonPage = function(p,d) {
             </div>
         </body>
     </html>
+        `
+    
+    return pagHTML
+}
+
+exports.genSexDistPage = function(pessoas, date) {
+    var dict_sexos = {}
+    for(let i = 0; i < pessoas.length; i++){
+        if (pessoas[i].sexo in dict_sexos){
+            dict_sexos[pessoas[i].sexo] += 1
+        }
+        else {
+            dict_sexos[pessoas[i].sexo] = 1
+        }
+    }
+
+
+
+    var pagHTML = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>About People...</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+        <body>
+            <div class="w3-card-4">
+                <header class="w3-container w3-purple">
+                    <h1>Distribuição por sexo</h1>
+                    <a class="w3-btn w3-round w3-white" href="/">Página Inicial</a>
+                    <a class="w3-btn w3-round" href="/pessoas/sexo">Distribuição por sexo</a>
+                    <a class="w3-btn w3-round" href="/pessoas/desporto">Distribuição por desporto</a>
+                    <a class="w3-btn w3-round" href="/pessoas/profissoes">Top 10 profissões</a>
+                </header>
+            
+            <div class="w3-container">
+                <table class="w3-table-all">
+                    <tr>
+                        <th>Sexo</th>
+                        <th>Frequência</th>
+                    </tr>
+            
+            
+                
+                `
+    for (let sexo in dict_sexos) {
+        pagHTML += `
+        <tr>
+            <td>${sexo}</td>
+            <td>
+                <a href="/pessoas/sexo/${sexo}">${dict_sexos[sexo]}</a>
+            </td>
+        </tr>
+        `
+    }
+
+
+    pagHTML += `
+                        </table>
+                    </div>
+                    <footer class="w3-container w3-blue">
+                        <h5>Generated in EngWeb2023 ${date}</h5>
+                    </footer>
+                </div>
+            </body>
+        </html>
+        `
+    
+    return pagHTML
+}
+
+
+exports.genSexListPage = function(list, parameter, date) {
+    var pagHTML = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>About People...</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+        <body>
+            <div class="w3-card-4">
+                <header class="w3-container w3-purple">
+                    <h1>Lista de pessoas do sexo: ${parameter}</h1>
+                    <a class="w3-btn w3-round w3-white" href="/">Página Inicial</a>
+                    <a class="w3-btn w3-round" href="/pessoas/sexo">Distribuição por sexo</a>
+                    <a class="w3-btn w3-round" href="/pessoas/desporto">Distribuição por desporto</a>
+                    <a class="w3-btn w3-round" href="/pessoas/profissoes">Top 10 profissões</a>
+                </header>
+            
+            <div class="w3-container">
+                <table class="w3-table-all">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Idade</th>
+                        <th>Sexo</th>
+                        <th>Cidade</th>
+                    </tr>
+            
+            
+                
+                `
+    for (let i = 0; i < list.length; i++) {
+        pagHTML += `
+        <tr>
+            <td>${list[i].id}</td>
+            <td>
+                <a href="/pessoas/${list[i].id}">${list[i].nome}</a>
+            </td>
+            <td>${list[i].idade}</td>
+            <td>${list[i].sexo}</td>
+            <td>${list[i].morada.cidade}</td>
+        </tr>
+        `
+    }
+
+
+    pagHTML += `
+                        </table>
+                    </div>
+                    <footer class="w3-container w3-blue">
+                        <h5>Generated in EngWeb2023 ${date}</h5>
+                    </footer>
+                </div>
+            </body>
+        </html>
+        `
+    
+    return pagHTML
+}
+
+
+exports.genSportDistPage = function(pessoas, date) {
+    var dict_sports = {}
+    for(let i = 0; i < pessoas.length; i++){
+        for(sport of pessoas[i].desportos) {
+            if (sport in dict_sports){
+                dict_sports[sport] += 1
+            }
+            else {
+                dict_sports[sport] = 1
+            }
+        }
+    }
+
+
+
+    var pagHTML = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>About People...</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+        <body>
+            <div class="w3-card-4">
+                <header class="w3-container w3-purple">
+                    <h1>Distribuição por desporto</h1>
+                    <a class="w3-btn w3-round w3-white" href="/">Página Inicial</a>
+                    <a class="w3-btn w3-round" href="/pessoas/sexo">Distribuição por sexo</a>
+                    <a class="w3-btn w3-round" href="/pessoas/desporto">Distribuição por desporto</a>
+                    <a class="w3-btn w3-round" href="/pessoas/profissoes">Top 10 profissões</a>
+                </header>
+            
+            <div class="w3-container">
+                <table class="w3-table-all">
+                    <tr>
+                        <th>Desporto</th>
+                        <th>Frequência</th>
+                    </tr>
+            
+            
+                
+                `
+    for (let sport in dict_sports) {
+        pagHTML += `
+        <tr>
+            <td>${sport}</td>
+            <td>
+                <a href="/pessoas/desporto/${sport}">${dict_sports[sport]}</a>
+            </td>
+        </tr>
+        `
+    }
+
+
+    pagHTML += `
+                        </table>
+                    </div>
+                    <footer class="w3-container w3-blue">
+                        <h5>Generated in EngWeb2023 ${date}</h5>
+                    </footer>
+                </div>
+            </body>
+        </html>
+        `
+    
+    return pagHTML
+}
+
+
+exports.genSportListPage = function(list, sport, date) {
+    var pagHTML = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>About People...</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+        <body>
+            <div class="w3-card-4">
+                <header class="w3-container w3-purple">
+                    <h1>Lista de pessoas que praticam o desporto: ${sport}</h1>
+                    <a class="w3-btn w3-round w3-white" href="/">Página Inicial</a>
+                    <a class="w3-btn w3-round" href="/pessoas/sexo">Distribuição por sexo</a>
+                    <a class="w3-btn w3-round" href="/pessoas/desporto">Distribuição por desporto</a>
+                    <a class="w3-btn w3-round" href="/pessoas/profissoes">Top 10 profissões</a>
+                </header>
+            
+            <div class="w3-container">
+                <table class="w3-table-all">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Idade</th>
+                        <th>Sexo</th>
+                        <th>Cidade</th>
+                    </tr>
+            
+            
+                
+                `
+    for (let i = 0; i < list.length; i++) {
+        if(list[i].desportos.includes(sport)) {
+            pagHTML += `
+            <tr>
+                <td>${list[i].id}</td>
+                <td>
+                    <a href="/pessoas/${list[i].id}">${list[i].nome}</a>
+                </td>
+                <td>${list[i].idade}</td>
+                <td>${list[i].sexo}</td>
+                <td>${list[i].morada.cidade}</td>
+            </tr>
+            `
+        }
+    }
+
+
+    pagHTML += `
+                        </table>
+                    </div>
+                    <footer class="w3-container w3-blue">
+                        <h5>Generated in EngWeb2023 ${date}</h5>
+                    </footer>
+                </div>
+            </body>
+        </html>
+        `
+    
+    return pagHTML
+}
+
+
+
+exports.genJobDistPage = function(pessoas, date) {
+    var dict_jobs = {}
+    for(let i = 0; i < pessoas.length; i++){
+        if (pessoas[i].profissao in dict_jobs){
+            dict_jobs[pessoas[i].profissao] += 1
+        }
+        else {
+            dict_jobs[pessoas[i].profissao] = 1
+        }
+    }
+
+
+
+    var pagHTML = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>About People...</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+        <body>
+            <div class="w3-card-4">
+                <header class="w3-container w3-purple">
+                    <h1>Top 10 Profissões</h1>
+                    <a class="w3-btn w3-round w3-white" href="/">Página Inicial</a>
+                    <a class="w3-btn w3-round" href="/pessoas/sexo">Distribuição por sexo</a>
+                    <a class="w3-btn w3-round" href="/pessoas/desporto">Distribuição por desporto</a>
+                    <a class="w3-btn w3-round" href="/pessoas/profissoes">Top 10 profissões</a>
+                </header>
+            
+            <div class="w3-container">
+                <table class="w3-table-all">
+                    <tr>
+                        <th>Profissão</th>
+                        <th>Frequência</th>
+                    </tr>
+            
+            
+                
+                `
+
+    var items = Object.keys(dict_jobs).map(function(key) {
+        return [key, dict_jobs[key]];
+    });
+    items.sort(function(first, second) {
+        return second[1] - first[1];
+    });
+
+    for (let job of items.slice(0,10)) {
+        pagHTML += `
+        <tr>
+            <td>${job[0]}</td>
+            <td>
+                <a href="/pessoas/profissoes/${job[0]}">${job[1]}</a>
+            </td>
+        </tr>
+        `
+    }
+
+
+    pagHTML += `
+                        </table>
+                    </div>
+                    <footer class="w3-container w3-blue">
+                        <h5>Generated in EngWeb2023 ${date}</h5>
+                    </footer>
+                </div>
+            </body>
+        </html>
+        `
+    
+    return pagHTML
+}
+
+
+exports.genJobListPage = function(list, job, date) {
+    var pagHTML = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="UTF-8"/>
+            <title>About People...</title>
+            <link rel="stylesheet" type="text/css" href="w3.css"/>
+        </head>
+        <body>
+            <div class="w3-card-4">
+                <header class="w3-container w3-purple">
+                    <h1>Lista de pessoas que tem como profissão: ${job}</h1>
+                    <a class="w3-btn w3-round w3-white" href="/">Página Inicial</a>
+                    <a class="w3-btn w3-round" href="/pessoas/sexo">Distribuição por sexo</a>
+                    <a class="w3-btn w3-round" href="/pessoas/desporto">Distribuição por desporto</a>
+                    <a class="w3-btn w3-round" href="/pessoas/profissoes">Top 10 profissões</a>
+                </header>
+            
+            <div class="w3-container">
+                <table class="w3-table-all">
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Idade</th>
+                        <th>Sexo</th>
+                        <th>Cidade</th>
+                    </tr>
+            
+            
+                
+                `
+    for (let i = 0; i < list.length; i++) {
+        pagHTML += `
+        <tr>
+            <td>${list[i].id}</td>
+            <td>
+                <a href="/pessoas/${list[i].id}">${list[i].nome}</a>
+            </td>
+            <td>${list[i].idade}</td>
+            <td>${list[i].sexo}</td>
+            <td>${list[i].morada.cidade}</td>
+        </tr>
+        `
+    
+    }
+
+
+    pagHTML += `
+                        </table>
+                    </div>
+                    <footer class="w3-container w3-blue">
+                        <h5>Generated in EngWeb2023 ${date}</h5>
+                    </footer>
+                </div>
+            </body>
+        </html>
         `
     
     return pagHTML
--- TPC3/pessoas-server.js
+++ TPC3/pessoas-server.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
     var d = new Date().toISOString().substring(0,16)
     console.log(req.method + " " + req.url + " " + d)
 
-    if(req.url == '/pessoas') {
+    if(req.url == '/pessoas' || req.url == "/") {
         axios.get('http://localhost:3000/pessoas')
             .then(function(resp) { // callback se conseguir fazer o pedido
                 var pessoas = resp.data
@@ -50,6 +50,7 @@ http.createServer(function (req, res) {
                 res.end('<p>Erro na obtenção de dados: ' + erro + '</p>')
             })
     }
+    
     else if(req.url.match(/w3\.css$/)) {
         fs.readFile("w3.css", function(erro,dados) {
             if(erro) {
@@ -61,6 +62,90 @@ http.createServer(function (req, res) {
                 res.end(dados)
             }
         })
+    }
+    else if(req.url == "/pessoas/sexo") {
+        console.log('Pedindo ' + req.url.substring(9))
+        axios.get('http://localhost:3000/pessoas/')
+            .then(function(resp) { // callback se conseguir fazer o pedido
+                var pessoas = resp.data
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end(mypages.genSexDistPage(pessoas, d))
+            })
+            .catch(erro => { // callback se não conseguir fazer o pedido
+                console.log('Erro: ' + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end('<p>Erro na obtenção de dados: ' + erro + '</p>')
+            })
+    }
+    else if(req.url.match(/\/pessoas\/sexo\/\w+/)) {
+        console.log('Pedindo ' + req.url.substring(9))
+        axios.get('http://localhost:3000/pessoas?sexo=' + req.url.substring(14))
+            .then(function(resp) { // callback se conseguir fazer o pedido
+                var pessoas = resp.data
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end(mypages.genSexListPage(pessoas,req.url.substring(14), d))
+            })
+            .catch(erro => { // callback se não conseguir fazer o pedido
+                console.log('Erro: ' + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end('<p>Erro na obtenção de dados: ' + erro + '</p>')
+            })
+    }
+    else if(req.url == "/pessoas/desporto") {
+        console.log('Pedindo ' + req.url.substring(9))
+        axios.get('http://localhost:3000/pessoas/')
+            .then(function(resp) { // callback se conseguir fazer o pedido
+                var pessoas = resp.data
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end(mypages.genSportDistPage(pessoas, d))
+            })
+            .catch(erro => { // callback se não conseguir fazer o pedido
+                console.log('Erro: ' + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end('<p>Erro na obtenção de dados: ' + erro + '</p>')
+            })
+    }
+    else if(req.url.match(/\/pessoas\/desporto\/\w+/)) {
+        console.log('Pedindo ' + req.url.substring(9))
+        axios.get('http://localhost:3000/pessoas')
+            .then(function(resp) { // callback se conseguir fazer o pedido
+                var pessoas = resp.data
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end(mypages.genSportListPage(pessoas,req.url.substring(18), d))
+            })
+            .catch(erro => { // callback se não conseguir fazer o pedido
+                console.log('Erro: ' + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end('<p>Erro na obtenção de dados: ' + erro + '</p>')
+            })
+    }
+    else if(req.url == "/pessoas/profissoes") {
+        console.log('Pedindo ' + req.url.substring(9))
+        axios.get('http://localhost:3000/pessoas/')
+            .then(function(resp) { // callback se conseguir fazer o pedido
+                var pessoas = resp.data
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end(mypages.genJobDistPage(pessoas, d))
+            })
+            .catch(erro => { // callback se não conseguir fazer o pedido
+                console.log('Erro: ' + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end('<p>Erro na obtenção de dados: ' + erro + '</p>')
+            })
+    }
+    else if(req.url.match(/\/pessoas\/profissoes\/\w+/)) {
+        console.log('Pedindo ' + req.url.substring(9))
+        axios.get('http://localhost:3000/pessoas?profissao=' + req.url.substring(20))
+            .then(function(resp) { // callback se conseguir fazer o pedido
+                var pessoas = resp.data
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end(mypages.genJobListPage(pessoas,req.url.substring(20), d))
+            })
+            .catch(erro => { // callback se não conseguir fazer o pedido
+                console.log('Erro: ' + erro)
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'} )
+                res.end('<p>Erro na obtenção de dados: ' + erro + '</p>')
+            })
     }
     else {
         res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'} )
